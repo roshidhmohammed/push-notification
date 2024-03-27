3ff2f3028433802c81b74c9040ca768c414bf200
roshidhmohammed <115979576+roshidhmohammed@users.noreply.github.com>
2024-03-28 00:24:28 +0530

    first commit

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,38 @@
+// This a service worker file for receiving push notifitications.
+// See `Access registration token section` @ https://firebase.google.com/docs/cloud-messaging/js/client#retrieve-the-current-registration-token
+
+// Scripts for firebase and firebase messaging
+importScripts('https://www.gstatic.com/firebasejs/10.9.0/firebase-app-compat.js')
+importScripts('https://www.gstatic.com/firebasejs/10.9.0/firebase-messaging-compat.js');
+
+
+// Initialize the Firebase app in the service worker by passing the generated config
+const firebaseConfig = {
+  apiKey: "AIzaSyB3Js8U0dzBEWJPaMWnQwkYqUux_mxXOkw",
+  authDomain: "pwa-pushnotification-6b518.firebaseapp.com",
+  projectId: "pwa-pushnotification-6b518",
+  storageBucket: "pwa-pushnotification-6b518.appspot.com",
+  messagingSenderId: "376539431131",
+  appId: "1:376539431131:web:901654eacbe9f2999f8cfe",
+  measurementId: "G-3NRXFNS8H9"
+};
+
+// let self = this
+
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
+
+// Handle incoming messages while the app is not in focus (i.e in the background, hidden behind other tabs, or completely closed).
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle,
+      notificationOptions);
+});
--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {ToastContainer, Zoom} from "react-toastify";
+import Notification from "./firebaseNotifications/Notification";
 
 function App() {
   return (
     <div className="App">
+       <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={true}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme='light'
+          transition={Zoom}
+          closeButton={false}
+      />
+        <Notification/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
