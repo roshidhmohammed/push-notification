a1d42a22a2c82338a3ada1391dadc056dc31d911
roshidhmohammed <115979576+roshidhmohammed@users.noreply.github.com>
2024-03-28 09:42:19 +0530

    sixth commit

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -28,7 +28,7 @@ const isLocalhost = Boolean(
 export function register(config) {
     if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
         // The URL constructor is available in all browsers that support SW.
-        const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href, "https://push-notification-wine.vercel.app/");
+        const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
         if (publicUrl.origin !== window.location.origin) {
             // Our service worker won't work if PUBLIC_URL is on a different origin
             // from what our page is served on. This might happen if a CDN is used to
@@ -37,7 +37,7 @@ export function register(config) {
         }
 
         window.addEventListener('load', () => {
-            const swUrl = `/worker.js`;
+            const swUrl = `../public/firebase-messaging-sw.js`;
             console.log("swUrl", swUrl)
 
             if (isLocalhost) {
