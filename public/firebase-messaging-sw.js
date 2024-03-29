846d3f4ede5f0fa62b6d8321199a836996170a5e
roshidhmohammed <115979576+roshidhmohammed@users.noreply.github.com>
2024-03-30 01:54:48 +0530

    forteen commit

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,31 +7,29 @@
 
 
 // Scripts for firebase and firebase messaging
-// importScripts('https://www.gstatic.com/firebasejs/10.10.0/firebase-app-compat.js')
-// importScripts('https://www.gstatic.com/firebasejs/10.10.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.9.0/firebase-app-compat.js')
+importScripts('https://www.gstatic.com/firebasejs/10.9.0/firebase-messaging-compat.js');
 
-import {  firebase  } from 'firebase/app';
-let self=this
 
 // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
-  apiKey: "AIzaSyB3Js8U0dzBEWJPaMWnQwkYqUux_mxXOkw",
-  authDomain: "pwa-pushnotification-6b518.firebaseapp.com",
-  projectId: "pwa-pushnotification-6b518",
-  storageBucket: "pwa-pushnotification-6b518.appspot.com",
-  messagingSenderId: "376539431131",
-  appId: "1:376539431131:web:901654eacbe9f2999f8cfe",
-  measurementId: "G-3NRXFNS8H9"
+  apiKey: "AIzaSyDni2e91OLdsKXb1zmIZAyr8gtSyBuVpOs",
+  authDomain: "push-notification-516d5.firebaseapp.com",
+  projectId: "push-notification-516d5",
+  storageBucket: "push-notification-516d5.appspot.com",
+  messagingSenderId: "124290176895",
+  appId: "1:124290176895:web:155c0c9c4b86b0f6722c4a",
+  measurementId: "G-HD131NGRD2"
 };
 
 
 const app = firebase.initializeApp(firebaseConfig);
 
 // Retrieve firebase messaging
-const messagings = firebase.messaging(app);
+const messaging = firebase.messaging(app);
 
 // Handle incoming messages while the app is not in focus (i.e in the background, hidden behind other tabs, or completely closed).
-messagings.onBackgroundMessage(function(payload) {
+messaging.onBackgroundMessage(function(payload) {
   console.log('Received background message ', payload);
   // Customize notification here
   const notificationTitle = payload.notification.title;
